000933ec2d12cd981482295345116cfecfeb24ff
asmaa-204 <asmaa.abozaid204@gmail.com>
2025-03-03 20:54:59 +0200

    drawing rectangle

--- 1-hello-triangle/assets/shaders/tri.vert
+++ 1-hello-triangle/assets/shaders/tri.vert
@@ -1,19 +1,12 @@
 #version 330
 out vec4 vertexColor;
 
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec4 color;
+
+uniform float time;
+
 void main () {
-    const vec3 points[3] = vec3[3] (
-        vec3(-0.5, -0.5, 0.0),
-        vec3(0.5, -0.5, 0.0),
-        vec3(0.0, 0.5, 0.0)
-    );
-
-    vec4 vertexColors[3] = vec4[3] (
-        vec4(1.0, 0.0, 0.0, 1.0),
-        vec4(0.0, 1.0, 0.0, 1.0),
-        vec4(0.0, 0.0, 1.0, 1.0)
-    );
-
-    gl_Position = vec4(points[gl_VertexID], 1.0);
-    vertexColor = vertexColors[gl_VertexID];
+    gl_Position = vec4(position, 1.0);
+    vertexColor = color;
 }
--- 1-hello-triangle/main.cpp
+++ 1-hello-triangle/main.cpp
@@ -68,9 +68,37 @@ int main(){
     glDeleteShader(vertexShader);
     glDeleteShader(fragShader);
 
+    const float vertices[4*7] = {
+        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0,
+        0.5, -0.5, 0.0,  0.0, 1.0, 0.0, 1.0,
+        -0.5, 0.5, 0.0,   0.0, 0.0, 1.0, 1.0,
+        0.5, 0.5, 0.0,  1.0, 1.0, 0.0, 1.0,
+    };
+
+    const uint16_t elements[6] = {0, 1, 2, 3, 1, 2};
+    
     GLuint vertexArray;
     glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
 
+    GLuint vertexBuffer;
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 7 * sizeof(float), vertices, GL_STATIC_DRAW);
+    
+    GLuint elementBuffer;
+    glGenBuffers(1, &elementBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint16_t), elements, GL_STATIC_DRAW);
+
+    GLuint positionLoc = 0;
+    glEnableVertexAttribArray(positionLoc);
+    glVertexAttribPointer(positionLoc, 3, GL_FLOAT, false, 7 * sizeof(float), nullptr);
+
+    GLuint colorLoc = 1;
+    glEnableVertexAttribArray(colorLoc);
+    glVertexAttribPointer(colorLoc, 4, GL_FLOAT, false, 7 * sizeof(float), (void *) (3*sizeof(float)));
+
+    GLint time_loc = glGetUniformLocation(program, "time");
     while (!glfwWindowShouldClose(window))
     {
         float time = (float)glfwGetTime();
@@ -80,7 +108,9 @@ int main(){
 
         glUseProgram(program);
         glBindVertexArray(vertexArray);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUniform1f(time_loc, time);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, (void *)0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
